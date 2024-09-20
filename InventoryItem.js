b4420fe30cff464b29eaf7feb4854e84495e0d3e
Ali Mousazada <mousazada.ali@gmail.com>
2024-09-19 20:47:27 -0400

    writing component and props

--- InventoryItem.js
+++ InventoryItem.js
@@ -1,10 +1,24 @@
-function InventoryItem (/* TODO: Take the props. Set defaults to the quantity. */)
-{
-	return (
-		<div>
-			<h2><!-- TODO: Render the item's details. --></h2>
-			<!-- TODO: Render the low stock alert based on the quantity of the item. -->
-			<!-- TODO: Render the high value alert based on the total value of the item. -->
-		</div>
-	);
+function InventoryItem({ name, type, quantity = 0, price = 0 }) {
+  const lowStockThreshold = 5;
+  const valueThreshold = 500;
+
+  const totalValue = price * quantity;
+
+  return (
+    <div>
+      <h3>
+        {name} ({type})
+      </h3>
+      {quantity < lowStockThreshold && (
+        <Message>
+          <p>Low Stock ! {quantity} remained .</p>
+        </Message>
+      )}
+      {totalValue > valueThreshold && (
+        <Message>
+          <p>High value - consider extra protection !</p>
+        </Message>
+      )}
+    </div>
+  );
 }
